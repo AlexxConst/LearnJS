8e01c4cf4ecf1fc898962a8227ffb44342d3812f
Alexander Suslin <asuslin01@gmail.com>
2022-08-21 18:58:42 +0300

    Make Func

--- js/script.js
+++ js/script.js
@@ -1,36 +1,51 @@
 'use strict';
 
-const movieDB = {
-    movies: [
-        'Логан',
-        'Лига справедливости',
-        'Ла-ла лэнд',
-        'Одержимость',
-        'Скотт Пилигрим против...',
-    ],
-};
+window.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            'Логан',
+            'Лига справедливости',
+            'Ла-ла лэнд',
+            'Одержимость',
+            'Скотт Пилигрим против...',
+        ],
+    };
 
-const adv = document.querySelectorAll('.promo__adv img'),
-    poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list');
+    const adv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
 
-adv.forEach(item => {
-    item.remove();
-});
+    addForm.addEventListener('submit', event => {
+        event.preventDefault();
 
-genre.textContent = 'драма';
+        const newFilm = addInput.value;
+        const favorite = checkbox.checked;
 
-poster.style.backgroundImage = 'url("img/bg.jpg")';
+        movieDB.movies.push(newFilm);
+        movieDB.movies.sort();
+    });
 
-movieList.innerHTML = '';
+    adv.forEach(item => {
+        item.remove();
+    });
 
-movieDB.movies.sort();
+    genre.textContent = 'драма';
 
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
+    poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+    movieList.innerHTML = '';
+
+    movieDB.movies.sort();
+
+    movieDB.movies.forEach((film, i) => {
+        movieList.innerHTML += `
         <li class="promo__interactive-item">${i + 1} ${film}
             <div class="delete"></div>
         </li>
     `;
+    });
 });
